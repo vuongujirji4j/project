05f7c51a1bca333738e51f81ef8922b2f1697b35
vuongujirji4j <151615636+vuongujirji4j@users.noreply.github.com>
2023-12-15 17:37:03 +0700

    Add files via upload

---
+++ a_connect.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "websitelogin";
+    $conn_npt = new mysqli("$servername","$username","$password","$dbname");
+/*    if($conn_npt->errno){
+        echo "Lỗi kết nối, ". $conn_npt->error;
+    }else{
+        echo "<h1> Xin chào, phan viết vượng - 2210900085 </h1>";
+    }*/
+?>
---
+++ a_signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPage</title>
+    <link rel="stylesheet" href="a_css.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+    <form method="post">
+        <h1>Sign Up</h1>
+        <div class="textbox">
+            <input type="text" placeholder="username">
+            <i class='bx bxs-user'></i>
+        </div>
+        <div class="textbox">
+            <input type="password" placeholder="password">
+            <i class='bx bxs-lock-alt' ></i>
+        </div>
+        <input type="submit" class="btn" value="Sign Up" name=login_btn>
+    </div>
+    </form>
+</body>
+</html>
