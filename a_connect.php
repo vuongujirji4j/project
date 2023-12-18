d13de9856215872fad3aedbe3efa432c8150843a
vuongujirji4j <151615636+vuongujirji4j@users.noreply.github.com>
2023-12-18 13:35:38 +0700

    Update a_connect.php

--- a_connect.php
+++ a_connect.php
@@ -4,9 +4,9 @@
     $username = "root";
     $password = "";
     $dbname = "websitelogin";
-    $conn_npt = new mysqli("$servername","$username","$password","$dbname");
-/*    if($conn_npt->errno){
-        echo "Lỗi kết nối, ". $conn_npt->error;
+    $conn_pvv = new mysqli("$servername","$username","$password","$dbname");
+/*    if($conn_pvv->errno){
+        echo "Lỗi kết nối, ". $conn_pvv->error;
     }else{
         echo "<h1> Xin chào, phan viết vượng - 2210900085 </h1>";
     }*/
